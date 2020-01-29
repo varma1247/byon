62f129b42a3329839eae210aaa80560592817aed
varma1247 <saivarma10@gmail.com>
2020-01-29 16:00:43 -0600

    added page navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,79 @@
 import React from "react";
 import logo from "../images/logo.png";
-import { blue } from "@material-ui/core/colors";
+import { NavLink, BrowserRouter } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div className="container">
-      <nav
-        className="navbar navbar-dark bg-dark navbar-expand-sm"
-        style={{ fontWeight: "bold", color: "white", borderRadius: "10px" }}
-      >
-        <a
-          className="navbar-brand mx-auto"
-          style={{ display: "flex", alignItems: "center", cursor:"pointer" }}
+    <BrowserRouter>
+      <div className="container">
+        <nav
+          className="navbar navbar-dark bg-dark navbar-expand-sm"
+          style={{ fontWeight: "bold", color: "white", borderRadius: "10px" }}
         >
-          <img
-            src={logo}
-            width="50"
-            height="50"
-            className="d-inline-block align-top"
-            alt=""
-            style={{ marginRight: "10px"}}
-          ></img>
-          BYON
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Train With Your Own Data
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Build Your Own
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+          <div
+            className="navbar-brand mx-auto"
+            style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+          >
+            <img
+              src={logo}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt=""
+              style={{ marginRight: "10px" }}
+            ></img>
+            BYON
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  exact
+                  to="/"
+                  activeClassName="active"
+                >
+                  ImageNet
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  exact
+                  to="/train"
+                  activeClassName="active"
+                >
+                  Train With Your Own Data
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  exact
+                  to="/build"
+                  activeClassName="active"
+                >
+                  Build Your Own
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </BrowserRouter>
   );
 };
 
