fbbeb55e418dd84d7d1cef979a0227d3aaec039b
varma1247 <saivarma10@gmail.com>
2020-01-29 18:50:11 -0600

    UI Changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Imageupload from "./components/Imageupload";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const Train=()=>{
+  return (
+    <div>
+      <h1>Train</h1>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Imageupload />
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Imageupload} exact/>
+          <Route path="/train" component={CircularProgress}/>
+         
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,77 +3,86 @@ import logo from "../images/logo.png";
 import { NavLink, BrowserRouter } from "react-router-dom";
 const Navbar = () => {
   return (
-    <BrowserRouter>
-      <div className="container">
-        <nav
-          className="navbar navbar-dark bg-dark navbar-expand-sm"
-          style={{ fontWeight: "bold", color: "white", borderRadius: "10px" }}
+    <div className="container">
+      <nav
+        className="navbar navbar-dark bg-dark navbar-expand-sm"
+        style={{ fontWeight: "bold", color: "white", borderRadius: "10px" }}
+      >
+        <NavLink
+          className="navbar-brand mx-auto"
+          style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+          to="/"
+          exact
         >
-          <div
-            className="navbar-brand mx-auto"
-            style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-          >
-            <img
-              src={logo}
-              width="50"
-              height="50"
-              className="d-inline-block align-top"
-              alt=""
-              style={{ marginRight: "10px" }}
-            ></img>
-            BYON
-          </div>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse justify-content-end"
-            id="navbarNav"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link"
-                  exact
-                  to="/"
-                  activeClassName="active"
-                >
-                  ImageNet
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link"
-                  exact
-                  to="/train"
-                  activeClassName="active"
-                >
-                  Train With Your Own Data
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link"
-                  exact
-                  to="/build"
-                  activeClassName="active"
-                >
-                  Build Your Own
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </BrowserRouter>
+          <img
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt=""
+            style={{ marginRight: "10px" }}
+          ></img>
+          <span className="d-none d-sm-inline">BYON</span>
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav" style={{ marginLeft: "30px" }}>
+            <li className="nav-item align-self-center">
+              <NavLink
+                className="nav-link"
+                exact
+                to="/"
+                activeClassName="active"
+              >
+                ImageNet
+              </NavLink>
+              {/* <a className="nav-link" href="#">
+                Build Your Own
+              </a> */}
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                exact
+                to="/train"
+                activeClassName="active"
+              >
+                Train With Your Own Data
+              </NavLink>
+              {/* <a className="nav-link" href="#">
+                Train With Your Own Data
+              </a> */}
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                exact
+                to="/build"
+                activeClassName="active"
+              >
+                Build Your Own
+              </NavLink>
+              {/* <a className="nav-link" href="#">
+                Build Your Own
+              </a> */}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
 
